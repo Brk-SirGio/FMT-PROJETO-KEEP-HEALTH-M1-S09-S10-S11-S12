948d9aae50de0de15bf6ad1331ffa3091a3f9c68
Brk-SirGio <sergioo069@hotmail.com>
2024-03-24 15:31:54 -0300

    feito

--- MODULO-DE-ANGULAR/KEEP HEALTH/Keep-Health/src/app/login/login.component.ts
+++ MODULO-DE-ANGULAR/KEEP HEALTH/Keep-Health/src/app/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
