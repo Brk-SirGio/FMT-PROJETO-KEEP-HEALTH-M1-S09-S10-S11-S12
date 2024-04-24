2fcf48c71af5b4dbd1dbf1f3ef8e2a7610899006
Brk-SirGio <sergioo069@hotmail.com>
2024-04-23 23:57:18 -0300

    validação e mudança de css nos campos obrigatorios

--- MODULO-DE-ANGULAR/KEEP HEALTH/Keep-Health/src/app/cadastro/cadastro.component.ts
+++ MODULO-DE-ANGULAR/KEEP HEALTH/Keep-Health/src/app/cadastro/cadastro.component.ts
@@ -1,62 +1,54 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink, FormsModule, RouterModule],
+  imports: [ReactiveFormsModule, RouterLink, RouterModule, CommonModule],
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.scss']
 })
 export class CadastroComponent {
-  nome: string | null = null;
-  email: string | null = null;
-  peso: number | null = null;
-  altura: number | null = null;
-  nascimento: Date | null = null;
-  senha: string | null = null;
-  confirmarSenha: string | null = null;
+  cadastroForm: FormGroup;
 
-  constructor(private router: Router) {}
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.cadastroForm = this.fb.group({
+      nomeCompleto: ['', Validators.required],
+      peso: [null],
+      altura: [null],
+      email: ['', [Validators.required, Validators.email]],
+      dataNascimento: [null],
+      senha: ['', [Validators.required, Validators.minLength(8)]],
+      confirmarSenha: ['', [Validators.required, Validators.minLength(8)]]
+    }, { validator: this.senhasCombinam });
+  }
 
-  private validar(): boolean {
-    if (!this.email || !this.senha || !this.confirmarSenha) {
-      return false;
-    }
-
-    if (this.senha !== this.confirmarSenha) {
-      return false;
-    }
-
-    if (!this.nascimento) {
-      return false;
-    }
-
-    return true;
+  private senhasCombinam(group: FormGroup): { [key: string]: any } | null {
+    const senha = group.controls['senha'].value;
+    const confirmarSenha = group.controls['confirmarSenha'].value;
+    return senha === confirmarSenha ? null : { 'senhasNaoCombinam': true };
   }
 
   cadastrarUsuario(): void {
-    if (!this.validar()) {
-      return;
+    if (this.cadastroForm.valid) {
+      const usuario = this.cadastroForm.value;
+      const usuarioParaSalvar = {
+        ...usuario,
+        emailUsuario: undefined,
+        confirmarSenhaUsuario: undefined,
+        senhaUsuario: undefined,
+        email: usuario.email,
+        senha: usuario.senha,
+        codigoUsuario: 'teste123' // Forçar adicionar o código do usuário
+      };
+      localStorage.setItem('usuario', JSON.stringify(usuarioParaSalvar));
+      this.router.navigate(['/login']);
+      alert('Usuário cadastrado com sucesso!');
+    } else {
+      // Exibir mensagem de erro
+      alert('Por favor, preencha todos os campos obrigatórios.');
     }
-
-    const usuario = {
-      nome: this.nome,
-      peso: this.peso,
-      altura: this.altura,
-      email: this.email,
-      nascimento: this.nascimento,
-      senha: this.senha,
-      codigoUsuario: 'teste123'
-    };
-
-    localStorage.setItem('usuario', JSON.stringify(usuario));
-
-    this.router.navigate(['/login']);
-
-    // Exibir mensagem de sucesso
-    alert('Usuário cadastrado com sucesso!');
   }
-
 }
